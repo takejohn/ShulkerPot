502811357696ffb5c833e582943fe8951309fd64
takejohn <105504345+takejohn@users.noreply.github.com>
2023-10-24 23:38:43 +0900

    Fix command "/sopen" to work even if "/sp off" has executed

--- src/main/java/jp/takejohn/shulkerpot/bukkit/commands/CommandSopen.java
+++ src/main/java/jp/takejohn/shulkerpot/bukkit/commands/CommandSopen.java
@@ -1,7 +1,6 @@
 package jp.takejohn.shulkerpot.bukkit.commands;
 
 import jp.takejohn.shulkerpot.bukkit.ShulkerBoxInventoryAdapter;
-import jp.takejohn.shulkerpot.bukkit.ShulkerPot;
 import jp.takejohn.shulkerpot.bukkit.entity.HumanEntities;
 import jp.takejohn.shulkerpot.bukkit.inventory.ItemStacks;
 import org.bukkit.command.Command;
@@ -15,8 +14,7 @@ public class CommandSopen implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (!(sender instanceof Player player) || !ShulkerPot.getPlugin().getUtilizingConfig().get(player)
-                || HumanEntities.isOpeningExternalInventory(player)) {
+        if (!(sender instanceof Player player) || HumanEntities.isOpeningExternalInventory(player)) {
             return false;
         }
         final ItemStack itemStack = player.getInventory().getItemInMainHand();
